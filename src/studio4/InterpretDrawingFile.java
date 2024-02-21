3fb2f56272db494fd510f859753367fcbf980f2a
surface <surface@LAPTOP-RT0TU485.dhcp.wustl.edu>
2024-02-21 16:00:34 -0600

    Finished

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,44 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
-	}
+		String shapeType = in.next();
+	
+		if (shapeType.equals("rectangle")) {
+		
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		Color black = new Color(red, green,blue);
+		StdDraw.setPenColor(black);
+		if (filled==true) {
+			StdDraw.filledRectangle(p1, p2, p3, p4);
+		}
+		else {
+			StdDraw.rectangle(p1, p2, p3, p4);
+		}
+		}
+		else if (shapeType.equals("ellipse")) {
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			boolean filled = in.nextBoolean();
+			double p1 = in.nextDouble();
+			double p2 = in.nextDouble();
+			double p3 = in.nextDouble();
+			double p4 = in.nextDouble();
+			Color black = new Color(red, green,blue);
+			StdDraw.setPenColor(black);
+			if (filled==true) {
+				StdDraw.filledEllipse(p1, p2, p3, p4);
+			}
+			else {
+				StdDraw.ellipse(p1, p2, p3, p4);
+			}
+		}
+}
 }
